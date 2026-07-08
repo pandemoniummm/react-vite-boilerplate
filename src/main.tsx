21693c1f5d26a8c695817d885fc11477856ddefc
jihwan <jhn.ff0000@gmail.com>
2026-07-08 21:45:40 +0900

    feat: 절대 경로(@/) 환경 구축
    - vite-tsconfig-paths package install
    - App.tsx 절대경로 적용
    - main.tsx 절대경로 컨벤션 관련 주석 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+/*
+@/ 절대경로 적용은 취향 차이!
+엔트리 파일 시각적 응집도를 위해 적용하지 않았음
+*/
+import './index.css';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
