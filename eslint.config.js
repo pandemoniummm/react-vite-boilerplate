79de53b56f0a52bf4ebb46abdfab619ced66da19
jihwan <jhn.ff0000@gmail.com>
2026-07-08 20:12:56 +0900

    chore: Prettier Formatter Setting & ESLint 충돌 방지 연동
    - prettier, eslint-config-prettier package install
    - .prettierrrc를 통한 프로젝트 전역 코드 스타일 rule 정의
    - eslint-config-prettier 적용 -> linter/formatter 역할 분리
    - .vscode/settings.json 생성, Format on Save 환경 구성

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,17 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   // 빌드 결과물 폴더는 린트 검사에서 제외
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     // JS, TS 공식 권장 규칙 일괄 적용 (배열 전개 구문)
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
 
     // 린트 적용할 파일 확장자 지정
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
 
     // 언어 환경 설정 (브라우저 전역 변수 허용 등)
     languageOptions: {
@@ -22,18 +23,20 @@ export default tseslint.config(
 
     // 사용할 외부 플러그인 등록
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
 
     // 세부 규칙 커스텀
     rules: {
       ...reactHooks.configs.recommended.rules,
       // React 컴포넌트만 export 하도록 강제 (Fast Refresh 지원 목적)
-      "react-refresh/only-export-components": [
-        "warn",
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
     },
   },
+  // 다른 모든 규칙 중 코드 스타일 관련 사항 덮어쓰기
+  eslintConfigPrettier
 );
