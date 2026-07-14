26ff9df3af9f0b665bab11d378089b1853ecd179
jihwan <jhn.ff0000@gmail.com>
2026-07-14 23:55:34 +0900

    feat: 전역 에러/로딩 처리를 위한 AsyncBoundary 가드 컴포넌트 구축
    - react-error-boundary 패키지 도입
    - 토스(Toss) 아키텍처 기반의 하이브리드 래퍼 설계 (세트 및 단품 사용 가능)
    - 컴포넌트 바렐 패턴(index.ts) 적용으로 임포트 경로 최적화

---
+++ src/components/common/LoadingFallback.tsx
@@ -0,0 +1,8 @@
+export default function LoadingFallback() {
+  // TODO: 프로젝트 시작 시 디자인에 맞춰 스피너나 스켈레톤 UI로 교체하세요.
+  return (
+    <div>
+      <div>Loading...</div>
+    </div>
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from './common';
+// 향후 Button, Modal 등 다른 공통 컴포넌트가 생기면 추가로 export
